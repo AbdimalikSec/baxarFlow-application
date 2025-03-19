034e489fa488e9a18ee951aba6f84b32287bf105
AbdimalikSec <saacidyuusuf871@gmail.com>
2025-03-19 06:02:19 +0300

    home change

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -46,22 +46,3 @@ const Home = () => {
 
 export default Home;
 
-
-
-/* const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId:  import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASURE_ID
-}; */
-
-/* VITE_FIREBASE_API_KEY="AIzaSyDYD4D4sSgktt8skWNlVDPjL27IbOMFy7M",
-VITE_FIREBASE_AUTH_DOMAIN="fashilhack.firebaseapp.com",
-VITE_FIREBASE_PROJECT_ID="fashilhack",
-VITE_FIREBASE_STORAGE_BUCKET="fashilhack.firebasestorage.app",
-VITE_FIREBASE_MESSAGING_SENDER_ID="26946334493",
-VITE_FIREBASE_APP_ID="1:26946334493:web:1f685d063661774bf6fbf4",
-VITE_FIREBASE_MEASURE_ID="G-E6HGX30XGN" */
